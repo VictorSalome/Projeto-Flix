6c41ff32eaa813127a7192ccb6ed9fc4ea299d47
Victor Salome <victorsalome41@hotmail.com>
2024-01-15 18:11:39 -0300

    Criação do tema e esquema de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,55 @@
-import React from 'react';
 
-import { ThemeProvider } from '@mui/system'
+
+import { ThemeProvider, Box, Typography } from '@mui/material'
+import { Header } from './components/Header';
+import { Layout } from './components/Layout';
+import { createTheme } from '@mui/material';
+import { apptheme } from './config/theme';
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+const theme = createTheme({})
+
+const Home = () => {
+  return (
+    <Typography variant='h1'>
+      Home
+    </Typography>
+  )
+}
+
+const About = () => {
+  return (
+    <Typography variant='h1'>
+      About
+    </Typography>
+  )
+}
+
 
 function App() {
-  return <ThemeProvider theme={{}}>
-    
+  return <ThemeProvider theme={apptheme}>
+    <Box
+      component="main"
+      sx={{
+        height: '100vh',
+        backgroundColor: (theme) => theme.palette.grey[900],
+      }}
+    >
+      <Header />s
+      <Layout>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </Layout>
+
+    </Box>
+
 
   </ThemeProvider>
 
---
+++ src/config/theme.ts
@@ -0,0 +1,19 @@
+import { createTheme } from "@mui/material";
+
+
+export const apptheme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#f5f5f1',
+        },
+        secondary: {
+            main: '#E50914',
+        },
+        text: {
+            primary: '#f5f5f1',
+        }
+
+    }
+
+})
